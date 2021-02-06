8e969aeb64984b3a88f2ace8de98e89405281f76
Your Name <you@example.com>
2021-02-06 11:21:09 -0800

    week2 finish, pre-workshop

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -9,28 +9,20 @@ class Contact extends Component {
     }
 
     render() {
-        return(
+        return (
             <ScrollView>
                 <Card
                     title="Contact Information"
-                    wrapperStyle={{margin:20}}
-                >
-                    
-                    {/* <Card.Title>Contact Information</Card.Title> reactnative docs */}
+                    wrapperStyle={{margin: 20}}>
                     <Text>1 Nucamp Way</Text>
                     <Text>Seattle, WA 98001</Text>
-                    <Text
-                        style={{marginBottom: 10 }}
-                    >
-                        U.S.A.
-                    </Text>
+                    <Text style={{marginBottom: 10}}>U.S.A.</Text>
                     <Text>Phone: 1-206-555-1234</Text>
                     <Text>Email: campsites@nucamp.co</Text>
                 </Card>
             </ScrollView>
         );
-    }
-
+    };
 }
 
 export default Contact;
